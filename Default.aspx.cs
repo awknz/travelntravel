b007eb2e3ecabcf2d6b74a6b7569ee428e4728d6
awknz <32289566+awknz@users.noreply.github.com>
2017-10-01 00:54:31 +0500

    Task: Contact Us With Email Configuration
    Addition of DataAccess and Business Layer
    Email configuration with contact us page/section.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Services;
+using EntitiesLayer;
+using BusinessLayer;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
+    }
+
+
+    [WebMethod]
+    public static bool sendEmail(Email obj)
+    {
+        EmailHandler emailobj = new EmailHandler();
+        obj.emailFrom = "awknz.ltd@gmail.com";
+        obj.password = "awknzdtl";
+        obj.subject = "Trave N Travel";
+        obj.body = obj.message;
+        obj.host = "smtp.gmail.com";
+        obj.port = 587;
+        
+        return emailobj.sendEmail(obj);
 
     }
 }
